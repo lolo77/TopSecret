92cff2b2a7e1e03018e3c742c4b0d6083ab76437
Florent FRADET <flohjk@gmail.com>
2023-03-14 21:18:53 +0100

    Released v1.6.0

--- src/com/topsecret/Main.java
+++ src/com/topsecret/Main.java
@@ -21,7 +21,7 @@ public class Main extends JFrame {
 
     private static final Log LOG = new Log(Main.class);
 
-    public static final String VERSION = "1.5.1";
+    public static final String VERSION = "1.6.0";
 
     private String inputFile = null;
     private String passM = null;
